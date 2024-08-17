2637f3c09a7f89da5d968cffab87b90c302ad4e8
cruzjeff225 <cruzjeff225>
2024-08-16 23:36:25 -0600

    Simulación de DB en JSON, lectura y escritura

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 const app = express();
 
 app.use(express.json());
 
-//Adicionando un endpoint
+//Ruta absoluta para db.json
+const tareasPath = path.join(__dirname, 'db.json');
+
+//Adicionando un endpoint (raíz)
 app.get("/", (req, res) => {
     res.send("Bienvenido a la API - Gestión de Tareas Simple")
 });
+
+//Lectura de DB simulada en JSON
+const readData = () => {
+    const data = fs.readFileSync("./db.json");
+    return JSON.parse(data);
+};
+
+//Escritura a DB simulada en JSON
+const writeData = (data) => {
+    fs.writeFileSync("./db.json", JSON.striginfy(data));
+    return JSON.parse(data);
+}
+
 
 app.listen(3000, () => {
     console.log('Servidor escuchando desde el puerto 3000');
